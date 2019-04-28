e103569bf6a3e04bf81e19fb8f367a71ff6d893d
4055020@qq.com <4055020@qq.com>
2019-04-29 04:44:20 +0800

    Update docker.sh

--- shell/docker/docker.sh
+++ shell/docker/docker.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update -y
-apt install docker-ce
+apt install docker-ce -y
 docker info
