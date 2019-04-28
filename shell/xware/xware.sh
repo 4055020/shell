23d16b9abeb12cb6c6d3bbb5f7f98831bedff51f
4055020@qq.com <4055020@qq.com>
2019-04-29 05:05:53 +0800

    Update xware.sh

--- shell/xware/xware.sh
+++ shell/xware/xware.sh
@@ -3,7 +3,6 @@
 docker pull senorsen/docker-thunder-xware
 mkdir -p /data/TDDOWNLOAD
 mkdir -p /data/cfg
-
 docker run -d --restart on-failure --name xware --net host -v /data/TDDOWNLOAD:/app/TDDOWNLOAD -v /data/config:/app/bin/cfg senorsen/docker-thunder-xware
 sleep 10s
 docker logs xware
