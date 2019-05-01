78cf0a9edf966343bdd8e7a92a6946af3212d715
4055020@qq.com <4055020@qq.com>
2019-05-01 19:10:20 +0800

    修正

---
+++ Other/test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Author:  yeho <lj2007331 AT gmail.com>
+# BLOG:  https://blog.linuxeye.cn
+#
+# Notes: OneinStack for CentOS/RadHat 6+ Debian 7+ and Ubuntu 12+
+#
+# Project home page:
+#       https://oneinstack.com
+#       https://github.com/lj2007331/oneinstack
+
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
+clear
+printf "
+#######################################################################
+#       OneinStack for CentOS/RadHat 6+ Debian 7+ and Ubuntu 12+      #
+#       For more information please visit https://oneinstack.com      #
+#######################################################################
+"
+# Check if user is root
+[ $(id -u) != "0" ] && { echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1; }
+echo "${CFAILURE}Error: You must be root to run this script${CEND}"; exit 1;
+
+function CheckDependence(){
+FullDependence='0';
+for BIN_DEP in `echo "$1" |sed 's/,/\n/g'`
+  do
+    if [[ -n "$BIN_DEP" ]]; then
+      Founded='0';
+      for BIN_PATH in `echo "$PATH" |sed 's/:/\n/g'`
+        do
+          ls $BIN_PATH/$BIN_DEP >/dev/null 2>&1;
+          if [ $? == '0' ]; then
+            Founded='1';
+            break;
+          fi
+        done
+      if [ "$Founded" == '1' ]; then
+        echo -en "[\033[32mok\033[0m]\t";
+      else
+        FullDependence='1';
+        echo -en "[\033[31mNot Install\033[0m]";
+      fi
+      echo -en "\t$BIN_DEP\n";
+    fi
+  done
+if [ "$FullDependence" == '1' ]; then
+  echo -ne "\n\033[31mError! \033[0mPlease use '\033[33mapt-get\033[0m' or '\033[33myum\033[0m' install it.\n\n\n"
+  exit 1;
+fi
+}
+
+CheckDependence wget,awk,grep,sed,cut,cat,cpio,gzip,find,dirname,basename,file,xz,git;
