ead9475409439530ce7fd49da46ed1caea6312c8
4055020 <50046297+4055020@users.noreply.github.com>
2019-05-01 22:47:25 +0800

    Update test.sh

--- Other/test.sh
+++ Other/test.sh
@@ -5,8 +5,8 @@ export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 clear
 printf "
 #######################################################################
-#       OneinStack for CentOS/RadHat 6+ Debian 7+ and Ubuntu 12+      #
-#       For more information please visit https://oneinstack.com      #
+#        4055020  for CentOS/RadHat 6+ Debian 7+ and Ubuntu 12+       #
+#     For more information please visit https://github.com/4055020    #
 #######################################################################
 "
 # Check if user is root
@@ -14,33 +14,43 @@ printf "
 
 check_sys(){
 	if [[ -f /etc/redhat-release ]]; then
-		Checkrelease="centos"
+		check_sys="centos"
 	elif cat /etc/issue | grep -q -E -i "debian"; then
-		Checkrelease="debian"
+		check_sys="debian"
 	elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-		Checkrelease="ubuntu"
+		check_sys="ubuntu"
 	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-		Checkrelease="centos"
+		check_sys="centos"
 	elif cat /proc/version | grep -q -E -i "debian"; then
-		Checkrelease="debian"
+		check_sys="debian"
 	elif cat /proc/version | grep -q -E -i "ubuntu"; then
-		Checkrelease="ubuntu"
+		check_sys="ubuntu"
 	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-		Checkrelease="centos"
+		check_sys="centos"
     fi
 	bit=`uname -m`
-}
 
-Installation(){
-	if [[ ${Checkrelease} = "centos" ]]; then
-		release="yum"
+	if [[ ${check_sys} = "centos" ]]; then
+		sh_sys="yum"
 	else
-		release="apt"
+		sh_sys="apt"
 	fi
 }
 
-function CheckDependence(){
-FullDependence='0';
+check_ver(){
+sh_new_ver=$(wget --no-check-certificate -qO- "https://raw.githubusercontent.com/4055020/shell/master/reok"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
+[[ -z ${sh_new_ver} ]] && echo "无法连接Github" && exit 0
+	if [[ ${sh_new_ver} != ${sh_ver} ]]; then
+		wget --no-check-certificate -qO /root/reok 'https://raw.githubusercontent.com/4055020/shell/master/reok'
+		cp -f /root/reok /usr/local/sbin/reok
+		chmod -x /usr/local/sbin/reok
+	else
+		echo "已连接到Github"
+	fi
+}
+
+function check_Dep(){
+Dependence='0';
 for BIN_DEP in `echo "$1" |sed 's/,/\n/g'`
   do
     if [[ -n "$BIN_DEP" ]]; then
@@ -56,20 +66,19 @@ for BIN_DEP in `echo "$1" |sed 's/,/\n/g'`
       if [ "$Founded" == '1' ]; then
         echo -en "[\033[32mok\033[0m]\t";
       else
-        FullDependence='1';
+        Dependence='1';
         echo -en "[\033[31mNot Install\033[0m]";
       fi
       echo -en "\t$BIN_DEP\n";
     fi
   done
-if [ "$FullDependence" == '1' ]; then
-	$release install -y $BIN_DEP
-#	echo -ne "\n\033[31mError! \033[0mPlease use '\033[33mapt-get\033[0m' or '\033[33myum\033[0m' install it.\n\n\n"
+if [ "$Dependence" == '1' ]; then
+	$sh_sys install -y $BIN_DEP
 	exit 1;
 fi
 }
 
-function httpRequest(){
+function check_shell(){
     info=`curl -s -m 10 --connect-timeout 10 -I https://raw.githubusercontent.com/4055020/shell/master/shell/$1/$1.sh`
     code=`echo $info|grep "HTTP"|awk '{print $2}'`
     if [ "$code" == "200" ];then
@@ -79,20 +88,7 @@ function httpRequest(){
     fi
 }
 
-check_shell(){
-sh_new_ver=$(wget --no-check-certificate -qO- "https://raw.githubusercontent.com/4055020/shell/master/reok"|grep 'sh_ver="'|awk -F "=" '{print $NF}'|sed 's/\"//g'|head -1)
-[[ -z ${sh_new_ver} ]] && echo "无法连接Github" && exit 0
-	if [[ ${sh_new_ver} != ${sh_ver} ]]; then
-		wget --no-check-certificate -qO /root/reok 'https://raw.githubusercontent.com/4055020/shell/master/reok'
-		cp -f /root/reok /usr/local/sbin/reok
-		chmod -x /usr/local/sbin/reok
-	else
-		echo "已连接到Github"
-	fi
-}
-
 check_sys
-Installation
-check_shell
-CheckDependence llljjj,wget,awk,grep,sed,cut,cat,cpio,gzip,find,dirname,basename,file,xz,git,llljjj;
-httpRequest "$1"
+check_Dep wget,awk,grep,sed,cut,cat,cpio,gzip,find,dirname,basename,file,xz,git;
+check_ver
+check_shell "$1"
