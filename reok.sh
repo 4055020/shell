dc9a7af8b3cda88056e6e76c5cc2fcaabdf90703
4055020 <50046297+4055020@users.noreply.github.com>
2019-05-02 14:04:12 +0800

    Update reok.sh

--- reok.sh
+++ reok.sh
@@ -2,16 +2,16 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
-Dependence=/reokey/install/Dependence
+Dependence=/reokey/install/Dependence.sh
 if [ ! -f $Dependence ];then
-	wget -O $Dependence https://raw.githubusercontent.com/4055020/shell/master/Dependence -T 5;
+	wget -O $Dependence https://raw.githubusercontent.com/4055020/shell/master/Dependence.sh -T 5;
 fi
 . $Dependence
 if [ ! -f $list ];then
-	wget -O $list https://raw.githubusercontent.com/4055020/shell/master/list -T 5;
+	wget -O $list https://raw.githubusercontent.com/4055020/shell/master/list.sh -T 5;
 fi
 . $list
-echo -e "${dco_g}当前系统为 Centos $dos_b位 请按任意键继续...主菜单+ / 退出-${dco_t}"
-read -p "" answer
+
+read -n1 -p "请按键继续" answer
 list "$answer"
 exit 0
